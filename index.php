8ff0ffeb776fd3c74c3abc1431e07aa24e773132
borvakarlett <borvakarlett@gmail.com>
2025-03-26 17:34:23 +0100

    footer added in separate file

--- index.php
+++ index.php
@@ -194,22 +194,10 @@
 
 </div> <!-- end of templatemo_wrapper -->
 
-<div id="templatemo_footer_wrapper">
- <div id="templatemo_footer">
-
-    <ul class="footer_menu">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Gallery</a></li>
-        <li class="last_menu"><a href="#">Contact</a></li>
-    </ul>
-
-    Copyright © 2048 <a href="#">Your Company Name</a> <!-- Credit: www.templatemo.com -->| Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
-    
-
-</div> <!-- end of footer -->
+<!--footer-->
+<?php
+    include('partials/footer.php');
+?>
 </div>
 <!-- templatemo 185 paper blog -->
 <!-- 
---
+++ partials/footer.php
@@ -0,0 +1,16 @@
+<div id="templatemo_footer_wrapper">
+ <div id="templatemo_footer">
+
+    <ul class="footer_menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li class="last_menu"><a href="#">Contact</a></li>
+    </ul>
+
+    Copyright © 2048 <a href="#">Your Company Name</a> <!-- Credit: www.templatemo.com -->| Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
+    
+
+</div> <!-- end of footer -->
