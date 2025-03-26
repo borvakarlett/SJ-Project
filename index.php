3b7a08527ef4295ea0e61b3cc339248670a34cd5
borvakarlett <borvakarlett@gmail.com>
2025-03-26 17:40:26 +0100

    header added in separate file

--- index.php
+++ index.php
@@ -12,20 +12,9 @@
 <div id="templatemo_wrapper">
     <div id="templatemo_left_column">
     
-    	<div id="site_title">
-            <h1><a href="#">Paper Blog<span>free web template</span></a></h1>
-        </div>
-        
-        <div id="templatemo_menu">
-            <ul>
-                <li><a href="#" class="current">Home</a></li>
-                <li><a href="#">Free Templates</a></li>
-                <li><a href="#">Web Gallery</a></li>
-                <li><a href="#">Testimonials</a></li>
-                <li><a href="#">Our Company</a></li>
-              	<li><a href="#">Contact Us</a></li>
-            </ul>    	
-        </div><!-- end of templatemo_menu -->
+    <?php
+        include('partials/header.php');
+    ?>
         
         <div id="templatemo_sidebar">
         
---
+++ partials/header.php
@@ -0,0 +1,16 @@
+<header>
+        <div id="site_title">
+            <h1><a href="#">Paper Blog<span>free web template</span></a></h1>
+        </div>
+        
+        <nav id="templatemo_menu">
+            <ul>
+                <li><a href="#" class="current">Home</a></li>
+                <li><a href="#">Free Templates</a></li>
+                <li><a href="#">Web Gallery</a></li>
+                <li><a href="#">Testimonials</a></li>
+                <li><a href="#">Our Company</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>    	
+        </nav>
+</header>
