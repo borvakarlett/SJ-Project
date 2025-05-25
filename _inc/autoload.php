e68c1f625698a48cd4bf9d4e6460c85e492a4af7
borvakarlett <borvakarlett@gmail.com>
2025-05-25 23:39:54 +0200

    changed auth to require

--- _inc/autoload.php
+++ _inc/autoload.php
@@ -1,6 +1,6 @@
 <?php
 include_once('_inc/classes/Article.php');
-include_once('_inc/classes/Auth.php');
+require_once('_inc/classes/Auth.php');
 include_once('_inc/classes/Comments.php');
 include_once('_inc/classes/Messages.php');
 require_once('_inc/classes/Database.php');
